c2dc3f6b67fd7474b444db2e03567f1ccca5df8d
dreehgoncalves <andressagoncalves1997@gmail.com>
2021-09-15 22:31:53 -0300

    criação cadastro

--- api/index.js
+++ api/index.js
@@ -1,19 +1,44 @@
-const express = require("express");
-const servidor = express();
-const mysql = require("mysql2");
+const express = require("express")
+const servidor = express()
+const mysql = require("mysql2")
 const banco = mysql.createPool({
     database: "3j_210915",
     user: "root",
     password: "",
     host: "localhost",
     port: "3306",
-});
+})
 const bodyParser = require('body-parser')
 
 servidor.use(bodyParser.urlencoded({ extended: false }))
 servidor.use(bodyParser.json())
-a
 
+servidor.post("/veiculo", (req, res, next) => {
+    let body = req.body;
+    const QUERY = `INSERT INTO veiculo (modelo, marca, preco_venda, proprietario) VALUES('${body.modelo}', '${body.marca}', ${body.preco_venda}, '${body.venda}'`;
+
+    banco.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({
+                Mensagem: "Erro no servidor"
+            })
+        }
+
+        conn.query(QUERY, (error, resultado) => {
+            conn.release()
+
+            if (error) {
+                return res.status(500).send({
+                    Mensagem: "Erro no servidor"
+                })
+            }
+
+            return res.status(200).send({
+                Mensagem: "Cadastro realizado com sucesso!",
+            })
+        })
+    })
+})
 
 /* teste de conexão */
 
@@ -23,25 +48,25 @@ servidor.get("/testarconexao", (req, res, next) => {
             return res.status(500).send({
                 Erro: "Erro no servidor",
                 Detalhes: error,
-            });
+            })
         }
 
-        conn.release();
+        conn.release()
 
         return res.status(200).send({
             Mensagem: "Conexão estabelecida com sucesso",
-        });
-    });
-});
+        })
+    })
+})
 
 servidor.get("/", (req, res, next) => {
     return res.send({
         mensagem: "Bem-vindo(a) ao servidor",
         cidade: "Itapeva",
         uf: "SP",
-    });
-});
+    })
+})
 
 servidor.listen(3000, () => {
-    console.log("Servidor funcionando!");
-});
+    console.log("Servidor funcionando!")
+})
