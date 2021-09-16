8785c73883be23d667f02f5167c1ddf7deaa4233
dreehgoncalves <andressagoncalves1997@gmail.com>
2021-09-16 00:10:03 -0300

    adição get

--- api/index.js
+++ api/index.js
@@ -15,12 +15,13 @@ servidor.use(bodyParser.json())
 
 servidor.post("/veiculo", (req, res, next) => {
     let body = req.body;
-    const QUERY = `INSERT INTO veiculo (modelo, marca, preco_venda, proprietario) VALUES('${body.modelo}', '${body.marca}', ${body.preco_venda}, '${body.venda}'`;
+    const QUERY = `INSERT INTO veiculo (modelo, marca, preco_venda, proprietario) VALUES('${body.modelo}', '${body.marca}', '${body.preco}', '${body.venda}'`;
 
     banco.getConnection((error, conn) => {
         if (error) {
             return res.status(500).send({
-                Mensagem: "Erro no servidor"
+                Mensagem: "Erro no servidor",
+                Detalhes: error
             })
         }
 
@@ -29,12 +30,43 @@ servidor.post("/veiculo", (req, res, next) => {
 
             if (error) {
                 return res.status(500).send({
-                    Mensagem: "Erro no servidor"
+                    Mensagem: "Erro no servidor",
+                    Detalhes: error
                 })
             }
 
             return res.status(200).send({
                 Mensagem: "Cadastro realizado com sucesso!",
+            })
+        })
+    })
+})
+
+servidor.get("/veiculo/:proprietario", (req, res, next) => {
+    let proprietario = req.params.proprietario;
+    const QUERY = `SELECT * FROM veiculo WHERE proprietario LIKE '%${proprietario}%' ORDER BY nome`;
+
+    banco.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({
+                Erro: "Não foi possível atender à solicitação",
+                Detalhes: error,
+            })
+        }
+
+        conn.query(QUERY, (error, resultado) => {
+            conn.release()
+
+            if (error) {
+                return res.status(500).send({
+                    Erro: "Não foi possível atender à solicitação",
+                    Detalhes: error,
+                })
+            }
+
+            return res.status(200).send({
+                Mensagem: "Consulta realizada com sucesso",
+                Dados: resultado,
             })
         })
     })
